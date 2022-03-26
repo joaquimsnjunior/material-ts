23750eee0d8c34604ef5a3102dca5ec110415759
joaquim silva <joaquimsilvanetojunior@gmail.com>
2022-03-26 12:37:58 -0300

    feat: Adicionado react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 // IGOB Solutions, Inc.
 // Create by Joaquim Silva on 25/03/2022
 
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 export const App = () => {
   return (
-    <div className="App">
-      Teste
-    </div>
-  )
+    <BrowserRouter>
+        <AppRoutes />
+    </BrowserRouter>
+  );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,14 @@
 // IGOB Solutions, Inc.
 // Create by Joaquim Silva on 25/03/2022
 
-export { };
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/pagina-inicial" element={<p>Página inicial</p>} />
+      
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
+  );
+}
