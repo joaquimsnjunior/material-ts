e480f304050bc12f92000f0ad533c98c577a7f63
joaquim silva <joaquimsilvanetojunior@gmail.com>
2022-03-26 13:19:50 -0300

    feat: Adicionado tema claro e providor

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 // IGOB Solutions, Inc.
 // Create by Joaquim Silva on 25/03/2022
 
+import { ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { LightTheme } from "./shared/themes"; 
 
 export const App = () => {
   return (
-    <BrowserRouter>
-        <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+          <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<Button>Taste</Button>} />
+      <Route path="/pagina-inicial" element={<Button variant='contained' color='primary'>Taste</Button>} />
       
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
