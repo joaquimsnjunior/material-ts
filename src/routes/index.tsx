23108c7b1b817ccfa49c56be67a53e6690dd0f9b
joaquim silva <joaquimsilvanetojunior@gmail.com>
2022-03-26 12:57:56 -0300

    feat: Adicionando mui.com

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 // IGOB Solutions, Inc.
 // Create by Joaquim Silva on 25/03/2022
 
+import { Button } from '@mui/material';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<p>Página inicial</p>} />
+      <Route path="/pagina-inicial" element={<Button>Taste</Button>} />
       
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
